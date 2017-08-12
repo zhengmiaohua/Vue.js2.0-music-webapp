bd38b7062cca47bfae708890d0fc7563fe552a7c
郑庙华 <496694940@qq.com>
2017-08-12 16:02:43 +0800

    add addsong page

--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -5,7 +5,7 @@
         <div class="list-header">
           <h1 class="title">
             <i class="icon" :class="iconMode" @click="changeMode"></i>
-            <span class="text"></span>
+            <span class="text">{{modeText}}</span>
             <span class="clear" @click="showConfirm"><i class="icon-clear"></i></span>
           </h1>
         </div>
@@ -26,7 +26,7 @@
           </transition-group>
         </scroll>
         <div class="list-operate">
-          <div class="add">
+          <div class="add" @click="addSong">
             <i class="icon-add"></i>
             <span class="text">添加歌曲到队列</span>
           </div>
@@ -36,6 +36,7 @@
         </div>
       </div>
       <confirm ref="confirm" @confirm="confirmClear"  text="是否清空播放列表" confirmBtnText="清空"></confirm>
+      <add-song ref="addSong"></add-song>
     </div>
   </transition>
 </template>
@@ -46,11 +47,13 @@ import Scroll from 'base/scroll/scroll'
 import {playMode} from 'common/js/config'
 import Confirm from 'base/confirm/confirm'
 import {playerMixin} from 'common/js/mixin'
+import AddSong from 'components/add-song/add-song'
   export default{
     mixins:[playerMixin],
     components:{
       Scroll,
-      Confirm
+      Confirm,
+      AddSong
     },
     data() {
       return {
@@ -58,9 +61,14 @@ import {playerMixin} from 'common/js/mixin'
       }
     },
     computed:{
-      
+      modeText() {
+        return this.mode===playMode.sequence ? '顺序播放' : this.mode===playMode.random ? '随机播放' : '单曲循环'
+      }
     },
     methods:{
+      addSong() {
+        this.$refs.addSong.show()
+      },
       confirmClear() {
         this.deleteSongList()
         this.hide()
