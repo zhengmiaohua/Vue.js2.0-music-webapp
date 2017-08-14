35ac6d1251a6e79b3009697da121efabe7c4bf61
郑庙华 <496694940@qq.com>
2017-08-14 12:21:15 +0800

    add user-center

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Search from 'components/search/search'
 import SingerDetail from 'components/singer-detail/singer-detail'
 import Disc from 'components/disc/disc'
 import TopList from 'components/top-list/top-list'
+import UserCenter from 'components/user-center/user-center'
 Vue.use(Router)
 
 export default new Router({
@@ -55,5 +56,9 @@ export default new Router({
         }
       ]
     },
+    {
+      path:'/user',
+      component:UserCenter
+    }
   ]
 })
